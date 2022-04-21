fef8b07340d396b9a70e4e34473d5d669f87f6b4
Свилен Топузов <Siskoy@students.softuni.bg>
2022-04-21 17:52:25 +0300

    Add solution to 'N choose K' problem

--- Combinatorial/Lab/NChooseKCount/Startup.cs
+++ Combinatorial/Lab/NChooseKCount/Startup.cs
@@ -1,12 +1,32 @@
-﻿namespace NChooseKCount
+﻿using System.Numerics;
+
+string? inputN = Console.ReadLine();
+string? inputK = Console.ReadLine();
+
+if (inputN is null || inputK is null) return;
+
+if (int.TryParse(inputN, out var n) == false)
 {
-    using System;
+    throw new ArgumentException("N is not an number!");
+}
 
-    public class Startup
+if (int.TryParse(inputK, out var k) == false)
+{
+    throw new ArgumentException("K is not an number");
+}
+
+BigInteger combinations = Factorial(n) / (Factorial(k) * Factorial(n - k));
+
+Console.WriteLine(combinations);
+
+static BigInteger Factorial(int n)
+{
+    BigInteger factorial = 1;
+
+    for (int i = 2; i <= n; i++)
     {
-        public static void Main()
-        {
-
-        }
+        factorial *= i;
     }
+
+    return factorial;
 }
