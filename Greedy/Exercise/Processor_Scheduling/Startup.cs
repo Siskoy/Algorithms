4c5c9d32e438d6a89212df6cb33a505279710b95
Свилен Топузов <Siskoy@students.softuni.bg>
2022-05-03 20:49:51 +0300

    Add Processor Scheduling solution

---
+++ Greedy/Exercise/Processor_Scheduling/Startup.cs
@@ -0,0 +1,41 @@
+﻿string? taskCountInput = Console.ReadLine();
+
+if (taskCountInput is null) return;
+
+int taskCount = int.Parse(taskCountInput.Replace("Tasks: ", ""));
+
+List<Task> tasks = new(taskCount);
+
+for (int i = 0; i < taskCount; i++)
+{
+    string? taskInput = Console.ReadLine();
+
+    if (taskInput is null) continue;
+
+    string[] taskParts = taskInput.Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+
+    if (taskParts.Length != 2) continue;
+
+    tasks.Add(new Task
+    {
+        Number = i + 1,
+        Value = int.Parse(taskParts[0]),
+        Deadline = int.Parse(taskParts[1])
+    });
+}
+
+int maxSteps = tasks.Max(t => t.Deadline);
+
+List<Task> optimalTasks = tasks
+    .OrderByDescending(t => t.Value)
+    .Take(maxSteps)
+    .OrderBy(t => t.Deadline)
+    .ToList();
+
+string scheduleOutput = string.Join(" -> ", optimalTasks.Select(t => t.Number));
+
+Console.WriteLine($"Optimal schedule: {scheduleOutput}");
+
+int totalValue = optimalTasks.Sum(t => t.Value);
+
+Console.WriteLine($"Total value: {totalValue}");
---
+++ Greedy/Exercise/Processor_Scheduling/Task.cs
@@ -0,0 +1,8 @@
+﻿internal class Task
+{
+    public int Number { get; set; }
+
+    public int Value { get; set; }
+
+    public int Deadline { get; set; }
+}
