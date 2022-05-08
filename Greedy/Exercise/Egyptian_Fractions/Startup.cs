5f1ad569fdb12db2455e74bd07945cf2cb28aec8
Свилен Топузов <Siskoy@students.softuni.bg>
2022-05-08 14:33:08 +0300

    Add Egyptian Fractions solution

---
+++ Greedy/Exercise/Egyptian_Fractions/Startup.cs
@@ -0,0 +1,44 @@
+﻿string? fractionInput = Console.ReadLine();
+
+if (fractionInput is null) return;
+
+string[] fractionParts = fractionInput.Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+if (fractionParts.Length != 2) return;
+
+long numerator = long.Parse(fractionParts[0]);
+long denominator = long.Parse(fractionParts[1]);
+
+if (numerator >= denominator)
+{
+    Console.WriteLine("Error (fraction is equal to or greater than 1)");
+    return;
+}
+
+HashSet<string> addedFractions = new();
+
+long index = 2;
+
+while (numerator != 0)
+{
+    long nextNumerator = numerator * index;
+    long indexNumerator = denominator;
+
+    long remaining = nextNumerator - indexNumerator;
+
+    if (remaining < 0)
+    {
+        index++;
+        continue;
+    }
+
+    numerator = remaining;
+    denominator *= index;
+
+    addedFractions.Add($"1/{index}");
+    index++;
+}
+
+string addedFractionsOutput = string.Join(" + ", addedFractions);
+
+Console.WriteLine($"{fractionInput} = {addedFractionsOutput}");
