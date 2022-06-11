fd3d760865bc0bbc636a3d1ea2ceb96c192f0f25
Свилен Топузов <Siskoy@students.softuni.bg>
2022-06-11 21:02:30 +0300

    Add Dynamic.Lab + Fibonacci solution

---
+++ Dynamic/Lab/Dynamic.Lab/Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿string? input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input)) return;
+
+int n = int.Parse(input);
+
+long[] found = new long[n + 1];
+
+long result = Fibonacci(n);
+
+Console.WriteLine(result);
+
+long Fibonacci(int n)
+{
+    if (found[n] != default) return found[n];
+
+    if (n == 0 || n == 1) return n;
+
+    long result = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+    found[n] = result;
+
+    return result;
+}
