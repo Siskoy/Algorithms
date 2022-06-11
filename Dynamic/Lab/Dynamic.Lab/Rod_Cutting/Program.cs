dd7a5528b85ecee7d4e1cfa002b99032a89974ab
Свилен Топузов <Siskoy@students.softuni.bg>
2022-06-11 22:07:53 +0300

    Add Rod Cutting solution

---
+++ Dynamic/Lab/Dynamic.Lab/Rod_Cutting/Program.cs
@@ -0,0 +1,37 @@
+﻿string? ropeInput = Console.ReadLine();
+string? ropeLengthInput = Console.ReadLine();
+
+if (ropeInput is null || ropeLengthInput is null) return;
+
+int[] rope = ropeInput.Split(" ").Select(int.Parse).ToArray();
+int ropeLength = int.Parse(ropeLengthInput);
+
+int[] bestPrice = new int[ropeLength + 1];
+int[] bestCombo = new int[ropeLength + 1];
+
+for (int i = 1; i <= ropeLength; i++)
+{
+    int currentBest = bestPrice[i];
+
+    for (int j = 1; j <= i; j++)
+    {
+        currentBest = Math.Max(currentBest, rope[j] + bestPrice[i - j]);
+
+        if (currentBest > bestPrice[i])
+        {
+            bestPrice[i] = currentBest;
+            bestCombo[i] = j;
+        }
+    }
+}
+
+Console.WriteLine(bestPrice[ropeLength]);
+
+while (ropeLength  - bestCombo[ropeLength] != 0)
+{
+    Console.Write(bestCombo[ropeLength] + " ");
+
+    ropeLength -= bestCombo[ropeLength];
+}
+
+Console.WriteLine(bestCombo[ropeLength]);
