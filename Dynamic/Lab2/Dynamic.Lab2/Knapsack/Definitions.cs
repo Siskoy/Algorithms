85582c8cb62bee3102ebc4aae323cfb769fecdcc
Свилен Топузов <Siskoy@students.softuni.bg>
2022-06-14 18:32:15 +0300

    Add Dynamic.Lab2 + Knapsack solution

---
+++ Dynamic/Lab2/Dynamic.Lab2/Knapsack/Definitions.cs
@@ -0,0 +1,65 @@
+﻿namespace Knapsack;
+
+public class Definitions
+{
+    public static Item[] FillKnapsack(List<Item> items, int capacity)
+    {
+        int[,] maxPrice = new int[items.Count, capacity + 1];
+        bool[,] isItemTaken = new bool[items.Count, capacity + 1];
+
+        // Fill first row
+        for (int c = 0; c <= capacity; c++)
+        {
+            if (items[0].Weight > c) continue;
+
+            maxPrice[0, c] = items[0].Price;
+            isItemTaken[0, c] = true;
+        }
+
+        // Fill rest rows
+        for (int i = 1; i < items.Count; i++)
+        {
+            for (int c = 0; c <= capacity; c++)
+            {
+                maxPrice[i, c] = maxPrice[i - 1, c];
+
+                int remainingCapacity = c - items[i].Weight;
+
+                if (remainingCapacity < 0) continue;
+
+                int possibleBestPrice = items[i].Price + maxPrice[i - 1, remainingCapacity];
+
+                if (possibleBestPrice > maxPrice[i, c])
+                {
+                    maxPrice[i, c] = possibleBestPrice;
+                    isItemTaken[i, c] = true;
+                }
+            }
+        }
+
+        List<Item> itemsTaken = new(items.Count);
+
+        // Trace back the result
+        for (int i = items.Count - 1; i >= 0; i--)
+        {
+            if (isItemTaken[i, capacity])
+            {
+                itemsTaken.Add(items[i]);
+                capacity -= items[i].Weight;
+            }
+        }
+
+        itemsTaken.Reverse();
+
+        return itemsTaken.ToArray();
+    }
+
+    public class Item
+    {
+        public string Name { get; set; } = default!;
+
+        public int Weight { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ Dynamic/Lab2/Dynamic.Lab2/Knapsack/Program.cs
@@ -0,0 +1,42 @@
+﻿using static Knapsack.Definitions;
+
+string? knapsackCapacityInput = Console.ReadLine();
+
+if (knapsackCapacityInput is null) return;
+
+int knapsackCapacity = int.Parse(knapsackCapacityInput);
+
+List<Item> items = new(knapsackCapacity);
+
+while (true)
+{
+    string? itemInput = Console.ReadLine();
+
+    if (itemInput is null) continue;
+
+    if (itemInput == "end") break;
+
+    string[] itemParts = itemInput.Split(" ");
+
+    Item newItem = new()
+    {
+        Name = itemParts[0],
+        Weight = int.Parse(itemParts[1]),
+        Price = int.Parse(itemParts[2])
+    };
+
+    items.Add(newItem);
+}
+
+Item[] itemsTaken = FillKnapsack(items, knapsackCapacity);
+
+double totalWeight = itemsTaken.Sum(i => i.Weight);
+double totalValue = itemsTaken.Sum(i => i.Price);
+
+Console.WriteLine($"Total Weight: {totalWeight}");
+Console.WriteLine($"Total Value: {totalValue}");
+
+foreach (var item in itemsTaken.OrderBy(i => i.Name))
+{
+    Console.WriteLine(item.Name);
+}
